e4a3f79ecf90ef9a1d26ee3aa99abbdc21095868
João Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-02-11 14:22:07 -0800

    particlejs config

---
+++ scripts/particleJs/app.js
@@ -0,0 +1,7 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'scripts/particleJs/assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'scripts/particleJs/assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
