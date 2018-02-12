fb52377a21e5f795b06e18cef1cff0711b02adde
João Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-02-11 19:16:32 -0800

    finished effects on education and interests

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,70 @@
+function detectmob() {
+    return !!(navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i));
+}
+
+
 $(function() {
+
     /* INIT SCROLLIFY CONFIG =========================================== */
+    let triggerPosition;
+    let marginValue = -11;
+    let marginValue2 = 24;
+    let lastHeight, height;
+
+    $(window).scroll(function() {
+
+        lastHeight = height;
+        //update height value
+        height = $(window).scrollTop();
+        console.log(height);
+
+        if(lastHeight < height) {
+            // console.log("moving scroll down");
+
+/* EXECUTE ONLY ON SCROLL DOWN =========================================== */
+            switch(detectmob()) {
+                case false:
+                    triggerPosition = 2000;
+                    break;
+                case true:
+                    triggerPosition = 0;
+                    break;
+            }
+
+            if((height  > triggerPosition) && triggerPosition > 0) {
+
+                if(marginValue <= 11){
+
+                    // do something
+                    $('.education-box-wrapper').css('margin-left', `${marginValue}rem`);
+                    marginValue += 0.2;
+
+                }
+
+                if(marginValue2 >= 0){
+
+                    // do something
+                    $('.interests-box-wrapper').css('margin-left', `${marginValue2}rem`);
+                    marginValue2 -= 0.2;
+
+                }
+
+
+            }
 
 
 
+        } else {
+            // console.log("moving scroll up");
+        }
 
+
+    });
 
 });
